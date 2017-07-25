4d6f3647607f318d9927521d8f7987a8cc959d13
xuhong <xuhong>
2017-07-25 17:55:34 +0800

    更新Fragment

---
+++ app/src/main/java/com/xuhong/smarthome/Fragment/FishPondFragment.java
@@ -0,0 +1,21 @@
+package com.xuhong.smarthome.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.xuhong.smarthome.R;
+
+
+public class FishPondFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_scene, null);
+        return view;
+    }
+}
