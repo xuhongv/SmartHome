3d483d6450ebc0af35d35019b49fc39f4a76bf8c
xuhong <xuhong>
2017-08-12 15:20:50 +0800

    新增 登录界面

---
+++ app/src/main/java/com/xuhong/smarthome/activity/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.xuhong.smarthome.activity;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.gyf.barlibrary.ImmersionBar;
+import com.squareup.picasso.Picasso;
+import com.xuhong.smarthome.R;
+import com.xuhong.smarthome.view.BlurTransformation;
+import com.xuhong.smarthome.view.CircleTransform;
+import com.xuhong.smarthome.view.PullScrollView;
+
+public class LoginActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initView();
+
+    }
+
+    private void initView() {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //ImmersionBar.setTitleBar(this, toolbar);
+        //设置toolbar初始透明度为0
+        //toolbar.getBackground().mutate().setAlpha(0);
+        ImageView ivHeaderBg = (ImageView) findViewById(R.id.ivHeaderBg);
+        ImageView ivmeIcon = (ImageView) findViewById(R.id.ivIcon);
+        PullScrollView pullView = (PullScrollView) findViewById(R.id.pullView);
+        pullView.setZoomView(ivHeaderBg);
+
+
+        Picasso.with(this)
+                .load(R.mipmap.ic_mine_header_bg)
+                .transform(new BlurTransformation(this))
+                .fit()
+                .into(ivHeaderBg);
+
+
+        Picasso.with(this)
+                .load(R.mipmap.ic_logo)
+                .transform(new CircleTransform())
+                .into(ivmeIcon);
+
+
+    }
+
+
+}
+
--- app/src/main/java/com/xuhong/smarthome/fragment/MineFragment.java
+++ app/src/main/java/com/xuhong/smarthome/fragment/MineFragment.java
@@ -1,5 +1,6 @@
 package com.xuhong.smarthome.fragment;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,17 +12,14 @@ import android.widget.RelativeLayout;
 import com.gyf.barlibrary.ImmersionBar;
 import com.squareup.picasso.Picasso;
 import com.xuhong.smarthome.R;
+import com.xuhong.smarthome.activity.LoginActivity;
 import com.xuhong.smarthome.view.BlurTransformation;
 import com.xuhong.smarthome.view.CircleTransform;
 import com.xuhong.smarthome.view.PullScrollView;
 
 
-public class MineFragment extends BaseFragment {
+public class MineFragment extends BaseFragment implements View.OnClickListener {
 
-
-    private ImageView ivHeaderBg, ivmeIcon;
-
-    private PullScrollView pullView;
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -37,9 +35,10 @@ public class MineFragment extends BaseFragment {
     @Override
     protected void initView(View view) {
 
-        ivHeaderBg = (ImageView) view.findViewById(R.id.ivHeaderBg);
-        ivmeIcon = (ImageView) view.findViewById(R.id.ivIcon);
-        pullView = (PullScrollView) view.findViewById(R.id.pullView);
+        ImageView ivHeaderBg = (ImageView) view.findViewById(R.id.ivHeaderBg);
+        ImageView ivmeIcon = (ImageView) view.findViewById(R.id.ivIcon);
+        ivmeIcon.setOnClickListener(this);
+        PullScrollView pullView = (PullScrollView) view.findViewById(R.id.pullView);
         pullView.setZoomView(ivHeaderBg);
 
         Picasso.with(getActivity())
@@ -55,5 +54,14 @@ public class MineFragment extends BaseFragment {
                 .into(ivmeIcon);
 
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ivIcon:
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+                break;
+        }
     }
 }
