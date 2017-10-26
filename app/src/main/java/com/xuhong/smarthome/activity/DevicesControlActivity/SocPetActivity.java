db57aa35f28c2df3e3d707b3e9eb3d17bb5097c7
半颗心脏 <870189248@qq.com>
2017-10-26 18:06:46 +0800

    10.26  增加了部分的宠物屋的ui同步代码

--- app/src/main/java/com/xuhong/smarthome/activity/DevicesControlActivity/SocPetActivity.java
+++ app/src/main/java/com/xuhong/smarthome/activity/DevicesControlActivity/SocPetActivity.java
@@ -1,38 +1,97 @@
 package com.xuhong.smarthome.activity.DevicesControlActivity;
 
 import android.graphics.Color;
+import android.os.Message;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 
+import com.gizwits.gizwifisdk.api.GizWifiDevice;
+import com.gizwits.gizwifisdk.enumration.GizWifiErrorCode;
 import com.xuhong.smarthome.R;
+import com.xuhong.smarthome.utils.L;
 import com.xuhong.smarthome.view.ColorCircularSeekBar;
 import com.xuhong.smarthome.view.ColorTempCircularSeekBar;
+import com.xuhong.smarthome.view.MotorControlView;
 import com.xuhong.smarthome.view.TemperatureView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Handler;
 
 public class SocPetActivity extends BaseDevicesControlActivity {
 
 
-    /**
-     * 色彩进度条
-     */
+    //色彩进度条
     private ColorCircularSeekBar circularSeekBar;
 
-    /**
-     * 色温进度条
-     */
+    //色温进度条
     private ColorTempCircularSeekBar colorTempCircularSeekBar;
 
-
     //湿度
-    private cn.fanrunqi.waveprogress.WaveProgressView WaveLoadingView;
-
+    private cn.fanrunqi.waveprogress.WaveProgressView mWaveLoadingView;
+    private TextView mTvHumidness;
 
     //温度
-    private TemperatureView temperatureView;
+    private TemperatureView mTemperatureView;
+    private TextView mTvTemper;
+
+    //马达
+    private MotorControlView mMotorView;
+    private Button mBtMotoReduce;
+    private Button mBtMotorAdd;
+
+    //灯的开关
+    private Switch mSwLight;
+
+    //红绿蓝亮度调节
+    private SeekBar mSbRed;
+    private SeekBar mSbGreen;
+    private SeekBar mSbBlue;
+
+    //按钮红绿蓝开关
+    private Button mBtRed;
+    private Button mBtGreen;
+    private Button mBtBlue;
+
+    //红外
+    private com.xuhong.smarthome.view.InfraredView mMInfraredView;
 
 
-    //ui
-    private Switch swRight;
+    //数据点相关
+    private static final String KEY_SWITCH = "LED_OnOff";
+    private static final String KEY_LIGHT_R = "LED_R";
+    private static final String KEY_LIGHT_B = "LED_B";
+    private static final String KEY_LIGHT_G = "LED_G";
+
+
+    //数据点临时存储的数值
+    private boolean tempSwitch = false;
+    private int tempLightRed = 0;
+    private int tempLightGreen = 0;
+    private int tempLightBlue = 0;
+
+
+    //Code
+    private static final int CODE_HANDLER_UI = 105;
+
+
+    private android.os.Handler mHandler = new android.os.Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case CODE_HANDLER_UI:
+                    updateColor();
+                    updateColortemp();
+                    break;
+            }
+
+        }
+    };
 
 
     @Override
@@ -42,7 +101,6 @@ public class SocPetActivity extends BaseDevicesControlActivity {
 
     @Override
     protected void bindView() {
-
 
         //色彩的进度条
         circularSeekBar = (ColorCircularSeekBar) findViewById(R.id.csbSeekbar2);
@@ -59,7 +117,7 @@ public class SocPetActivity extends BaseDevicesControlActivity {
 
         });
         //色温的进度条
-        colorTempCircularSeekBar = (ColorTempCircularSeekBar) findViewById(R.id.csbSeekbar);//色温
+        colorTempCircularSeekBar = (ColorTempCircularSeekBar) findViewById(R.id.csbSeekbar);
         colorTempCircularSeekBar.postInvalidateDelayed(2000);
         colorTempCircularSeekBar.setMaxProgress(100);
         colorTempCircularSeekBar.setProgress(30);
@@ -76,23 +134,33 @@ public class SocPetActivity extends BaseDevicesControlActivity {
 
 
         //湿度
-        WaveLoadingView = (cn.fanrunqi.waveprogress.WaveProgressView) findViewById(R.id.WaveLoadingView);
-        WaveLoadingView.setCurrent(28, "28%/良好");
+        mWaveLoadingView = (cn.fanrunqi.waveprogress.WaveProgressView) findViewById(R.id.WaveLoadingView);
+        mWaveLoadingView.setCurrent(28, "28%/良好");
 
 
         //温度
-        temperatureView = (TemperatureView) findViewById(R.id.temperatureView);
-        temperatureView.setProgress(26);
-
+        mTemperatureView = (TemperatureView) findViewById(R.id.temperatureView);
 
         //rgb灯
-        swRight= (Switch) findViewById(R.id.swRight);
-        swRight.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        mSwLight = (Switch) findViewById(R.id.swLight);
+        mSwLight.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 
             }
         });
+
+        //马达
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = -5; i < 6; i++) {
+            integerList.add(i);
+        }
+
+        mMotorView = (MotorControlView) findViewById(R.id.mMotorView);
+        mMotorView.setValueList(integerList);
+        mMotorView.setValue(5, null, 500);
+
+
     }
 
     /* 色彩*/
@@ -116,19 +184,61 @@ public class SocPetActivity extends BaseDevicesControlActivity {
         sendRgbCmd("LED_R", red,
                 "LED_G", green, "LED_B", blue);
 
-//        color_num_r = Color.red(color);
+//        data_R = Color.red(color);
 //        color_num_g = Color.green(color);
 //        color_num_b = Color.blue(color);
     }
 
     /* 色温*/
     public void cColorTemp(int color_num) {
-//        sendRgbCmd("Temperature_R", Color.red(color_num),
-//                "Temperature_G", Color.green(color_num), "Temperature_B", Color.blue(color_num));
+        sendRgbCmd("Temperature_R", Color.red(color_num), "Temperature_G", Color.green(color_num), "Temperature_B", Color.blue(color_num));
+
 //        color_num_temp_r = Color.red(color_num);
 //        color_num_temp_g = Color.green(color_num);
 //        color_num_temp_b = Color.blue(color_num);
     }
 
+    /*
+     * 更新色彩
+	 */
+    private void updateColor() {
+        circularSeekBar.setInnerColor(Color.argb(255, tempLightRed, tempLightGreen, tempLightBlue));
 
+    }
+
+    /*
+     * 更新色温
+     */
+    private void updateColortemp() {
+        colorTempCircularSeekBar.setInnerColor(Color.argb(255, tempLightRed, tempLightGreen, tempLightBlue));
+
+    }
+
+
+    @Override
+    protected void didReceiveData(GizWifiErrorCode result, GizWifiDevice device, ConcurrentHashMap<String, Object> dataMap, int sn) {
+        super.didReceiveData(result, device, dataMap, sn);
+        L.e("收到的数据：" + dataMap);
+
+        if (dataMap.get("data") != null) {
+            ConcurrentHashMap<String, Object> map = (ConcurrentHashMap<String, Object>) dataMap.get("data");
+            for (String dataKey : map.keySet()) {
+                if (dataKey.equals(KEY_SWITCH)) {
+                    tempSwitch = (Boolean) map.get(dataKey);
+                }
+
+                if (dataKey.equals(KEY_LIGHT_R)) {
+                    tempLightRed = (Integer) map.get(dataKey);
+                }
+                if (dataKey.equals(KEY_LIGHT_G)) {
+                    tempLightGreen = (Integer) map.get(dataKey);
+                }
+                if (dataKey.equals(KEY_LIGHT_B)) {
+                    tempLightBlue = (Integer) map.get(dataKey);
+                }
+
+            }
+        }
+
+    }
 }
---
+++ app/src/main/java/com/xuhong/smarthome/view/InfraredView.java
@@ -0,0 +1,219 @@
+package com.xuhong.smarthome.view;
+
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.xuhong.smarthome.R;
+
+public class InfraredView extends View {
+
+    private static final String TAG = "WaveView";
+
+    private int waveColor;
+
+    private int waveCount;
+
+    private Bitmap waveCenterIcon;
+
+    private Paint paint;
+
+    private int mWidth;
+
+    private int mHeight;
+
+    private int centerX;
+
+    private int centerY;
+
+    private float radius;    // 最外圆半径，即最大半径
+
+    private float innerRadius;  // 最内圆的半径，即最小半径
+
+    private int centerIconWidth;
+
+    private int centerIconHeight;
+
+    private float[] waveDegreeArr;
+
+    private boolean isRunning = true;
+    private Paint mTextPaint;
+
+    //显示的文字
+    private String showText = "正在感应...";
+
+    public InfraredView(Context context) {
+        this(context, null);
+    }
+
+    public InfraredView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        readAttrs(context, attrs);
+        init();
+    }
+
+    private void init() {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(waveColor);
+        paint.setStyle(Paint.Style.FILL);
+        waveDegreeArr = new float[waveCount];
+
+        // 设置中间 drawable 点击事件
+
+
+    }
+
+    private void readAttrs(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.InfraredView);
+        try {
+            waveColor = typedArray.getColor(R.styleable.InfraredView_waveColor, 0xffff0000);
+            waveCount = typedArray.getInt(R.styleable.InfraredView_waveCount, 4);
+            showText = typedArray.getString(R.styleable.InfraredView_waveText);
+            Drawable centerDrawable = typedArray.getDrawable(R.styleable.InfraredView_waveCenterIcon);
+            waveCenterIcon = ((BitmapDrawable) centerDrawable).getBitmap();
+        } catch (Exception e) {
+
+        } finally {
+            typedArray.recycle();
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
+        centerX = mWidth / 2;
+        centerY = mHeight / 2;
+        radius = Math.min(mWidth, mHeight) / 2f;
+        centerIconWidth = waveCenterIcon.getWidth();
+        centerIconHeight = waveCenterIcon.getHeight();
+        innerRadius = Math.max(centerIconWidth, centerIconHeight) * 1.2f;
+
+        for (int i = 0; i < waveCount; i++) {
+            waveDegreeArr[i] = innerRadius + (radius - innerRadius) / waveCount * i;
+        }
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        if (widthMode == MeasureSpec.UNSPECIFIED || widthMode == MeasureSpec.AT_MOST) {
+            widthMeasureSpec = MeasureSpec.makeMeasureSpec(dp2Px(120), MeasureSpec.EXACTLY);
+        }
+        if (heightMode == MeasureSpec.UNSPECIFIED || heightMode == MeasureSpec.AT_MOST) {
+            heightMeasureSpec = MeasureSpec.makeMeasureSpec(dp2Px(120), MeasureSpec.EXACTLY);
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        drawWave(canvas);
+        drawCenterCircle(canvas);
+        drawCenterText(canvas);
+
+    }
+
+    private void drawCenterCircle(Canvas canvas) {
+        canvas.drawCircle(centerX, centerY, innerRadius, paint);
+    }
+
+    private void drawWave(Canvas canvas) {
+        for (int i = 0; i < waveCount; i++) {
+            paint.setAlpha((int) (255 - 255 * waveDegreeArr[i] / radius));
+            canvas.drawCircle(centerX, centerY, waveDegreeArr[i], paint);
+        }
+        for (int i = 0; i < waveDegreeArr.length; i++) {
+            if ((waveDegreeArr[i] += 4) > radius) {
+                waveDegreeArr[i] = innerRadius;
+            }
+        }
+        if (isRunning) {
+            postInvalidateDelayed(50);
+        }
+    }
+
+    private void drawCenterText(Canvas canvas) {
+        mTextPaint = new Paint();
+        mTextPaint.setTextSize(30);
+        mTextPaint.setColor(getResources().getColor(R.color.black0));
+        int left = centerX - (int) getFontlength(mTextPaint, showText) / 2;
+        int top = centerY + (int) getFontHeight(mTextPaint) / 2;
+        canvas.drawText(showText, left, top, mTextPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_UP:
+                // 处理事件逻辑
+                handleEvent(event);
+                return true;
+        }
+        return true;
+    }
+
+    private void handleEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        Log.i(TAG, "handleEvent: " + "(" + touchX + "," + touchY + ")");
+        float distanceX = Math.abs(touchX - centerX);
+        float distanceY = Math.abs(touchY - centerY);
+        // 计算触摸点距离中心点的距离
+        float distance = (float) Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+        // 当点击的点距离中心点距离小于最内圆半径时，认为是点击有效，否则无效
+        if (distance < innerRadius) {
+            if (listener != null) {
+                listener.onCenterWaveClick();
+            }
+        }
+    }
+
+    OnCenterWaveClickListener listener;
+
+    public interface OnCenterWaveClickListener {
+        void onCenterWaveClick();
+    }
+
+    public void setOnCenterWaveClickListener(OnCenterWaveClickListener listener) {
+        this.listener = listener;
+    }
+
+    public void toggle() {
+        isRunning = !isRunning;
+        invalidate();
+    }
+
+    public boolean isWaveRunning() {
+        return isRunning;
+    }
+
+
+    private int dp2Px(int dpValue) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpValue, getResources().getDisplayMetrics());
+    }
+
+
+    public float getFontlength(Paint paint, String str) {
+        return paint.measureText(str);
+    }
+
+    public float getFontHeight(Paint paint) {
+        Paint.FontMetrics fm = paint.getFontMetrics();
+        return fm.descent - fm.ascent;
+    }
+}
